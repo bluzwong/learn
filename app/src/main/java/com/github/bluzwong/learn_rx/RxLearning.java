40cef24deab9ee92128a46d592da10b1c950cd61
bluzwong <bluzwong@wm>
2016-03-24 16:44:51 +0800

    使用rxjava的一种情形

--- app/src/main/java/com/github/bluzwong/learn_rx/RxLearning.java
+++ app/src/main/java/com/github/bluzwong/learn_rx/RxLearning.java
@@ -5,60 +5,38 @@ import rx.Single;
 import rx.functions.Action1;
 import rx.functions.Func1;
 
-/**
- * Created by wangzhijie on 2015/10/15.
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class RxLearning {
+
     public static void main(String[] args) {
-       /* // 最简单的
-        Observable.just("cccf")
-                .subscribe(System.out::println);
 
-        // range -> 从0到9的list
-        // buffer -> 2：每2个一组 3：跳过第3个
-        Observable.range(0, 10)
-                .buffer(2, 3)
-                .map(s -> "range + buffer : " + s)
-                .subscribe(System.out::println);
+        List<Integer> dataList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            dataList.add(new Random().nextInt(100));
+        }
 
-        //? why
-        Observable.interval(1, TimeUnit.SECONDS)
-                .buffer(3, TimeUnit.SECONDS)
-                .map(s -> "interval : " + s)
-                .subscribe(System.out::println);
-*/
-        ///? 不是很会用
-        /*Observable.range(0, 10)
-                .groupBy(i -> i % 2)
-                .subscribe(g -> {
-                    g.count().subscribe(integer -> {
-                        String str = "key: " + g.getKey() + "contains: " + integer;
-                        System.out.println(str);
-                    });
 
-                });*/
+        for (int i = 0; i < dataList.size(); i++) {
+            System.out.println(i + " => " + dataList.get(i));
+        }
 
-        // 跳过出错的数据
-        Observable.just(1,2,0,3,4,5)
-                //.map(i -> 100 / i)
-                .flatMap(RxLearning::divide)
-                .subscribe(System.out::println, Throwable::printStackTrace);
+        // todo 将 dataList 中偶数及重复项过滤掉，并且按从小到大排序
+        usingRxJava(dataList);
     }
 
-    private static Observable<Integer> divide(int number) {
-        return Observable.just(number)
-                .map(i -> {
-                    if (i == 4) {
-                        throw new RuntimeException();
+    private static void usingRxJava(List<Integer> dataList) {
+        Observable.from(dataList)
+                .filter(integer -> integer % 2 == 0)
+                .distinct()
+                .toSortedList((i1, i2) -> i1 - i2)
+                .subscribe(integers -> {
+                    for (Integer integer : integers) {
+                        System.out.println("after => " + integer);
                     }
-                    return 100 / i;
-                })
-                // 在这个observable出错时发射空
-                .onErrorResumeNext(throwable -> {
-                    if (throwable instanceof ArithmeticException) {
-                        return Observable.empty();
-                    }
-                    return Observable.error(throwable);
                 });
     }
+
 }
